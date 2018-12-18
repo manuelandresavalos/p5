0af2130e505aa7982cd2f7a2c08da8ccc34bd5db
manuelandresavalos <manuelandresavalos@gmail.com>
2018-12-18 01:32:42 -0300

    Now the maze is more clear and cell has the responsability to show yourself and draw its walls

--- maze_two/cell.js
+++ maze_two/cell.js
@@ -26,25 +26,45 @@ class Cell {
       x1: this.x,
       y1: this.y,
       x2: this.x + this.size,
-      y2: this.y
+      y2: this.y,
+      show: true
     };
     this.wallRight = {
       x1: this.x + this.size,
       y1: this.y,
       x2: this.x + this.size,
-      y2: this.y + this.size
+      y2: this.y + this.size,
+      show: true
     };
     this.wallDown = {
       x1: this.x + this.size,
       y1: this.y + this.size,
       x2: this.x,
-      y2: this.y + this.size
+      y2: this.y + this.size,
+      show: true
     };
     this.wallLeft = {
       x1: this.x,
       y1: this.y + this.size,
       x2: this.x,
-      y2: this.y
+      y2: this.y,
+      show: true
     };
+  }
+
+  show(){
+    stroke(this.color.r, this.color.g, this.color.b);
+    if(this.wallUp.show) {
+      line(this.wallUp.x1, this.wallUp.y1, this.wallUp.x2, this.wallUp.y2);
+    }
+    if(this.wallRight.show) {
+      line(this.wallRight.x1, this.wallRight.y1, this.wallRight.x2, this.wallRight.y2);
+    }
+    if(this.wallDown.show) {
+      line(this.wallDown.x1, this.wallDown.y1, this.wallDown.x2, this.wallDown.y2);
+    }
+    if(this.wallLeft.show) {
+      line(this.wallLeft.x1, this.wallLeft.y1, this.wallLeft.x2, this.wallLeft.y2);
+    }
   }
 }
--- maze_two/maze.js
+++ maze_two/maze.js
@@ -20,16 +20,7 @@ class Maze {
       y: ((canvasHeight - (this.rows * this.cellSize)) / 2)
     }
     translate(pos.x, pos.y)
-    //return pos;
   };
-
-  binaryRandom(){
-    return getRandomInt(0, 1);
-  }
-
-  changeColorCell(rgbObj){
-    fill(rgbObj.r, rgbObj.g, rgbObj.b);
-  }
 
   generate(){
     for (var row = 0; row < this.rows; row++) {
@@ -37,7 +28,6 @@ class Maze {
       for (var col = 0; col < this.cols; col++) {
         var cell = new Cell({x: col, y:row, size:this.cellSize});
         this.cellMap[row][col] = cell;
-        console.log(this.cellMap[row][col]);
       }
     }
   };
@@ -46,19 +36,11 @@ class Maze {
     //code here
   }
 
-  drawCell(cell){
-    stroke(cell.color.r, cell.color.g, cell.color.b);
-    line(cell.wallUp.x1, cell.wallUp.y1, cell.wallUp.x2, cell.wallUp.y2);
-    line(cell.wallRight.x1, cell.wallRight.y1, cell.wallRight.x2, cell.wallRight.y2);
-    line(cell.wallDown.x1, cell.wallDown.y1, cell.wallDown.x2, cell.wallDown.y2);
-    line(cell.wallLeft.x1, cell.wallLeft.y1, cell.wallLeft.x2, cell.wallLeft.y2);
-  }
-
   show(){
     for (var row = 0; row < this.cellMap.length; row++) {
       for (var col = 0; col < this.cellMap[row].length; col++) {
         // Draw the cell in canvas
-        this.drawCell(this.cellMap[row][col]);
+        this.cellMap[row][col].show();
       }
     }
   }
