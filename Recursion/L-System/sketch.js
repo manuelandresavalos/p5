3a88fbcf3227c8d460d3858db5f7c16b10b4a6ec
manuelandresavalos <manuelandresavalos@gmail.com>
2019-01-04 00:02:44 -0300

    Refactoring L-System

--- Recursion/L-System/sketch.js
+++ Recursion/L-System/sketch.js
@@ -22,20 +22,24 @@ Rules: (A -> AB), (B -> A)
 */
 
 var axiom = "A";
-var rule1 = { a: "A", b: "AB" };
-var rule2 = { a: "B", b: "A" };
 var sentence = axiom;
+var rules = [];
+rules[0] = { a: "A", b: "AB" };
+rules[1] = { a: "B", b: "A" };
 
 function generate() {
 	var nextSentence = "";
 	for (var i = 0; i < sentence.length; i++) {
 		var current = sentence.charAt(i);
-		if (current == rule1.a) {
-			nextSentence += rule1.b;
-		} else if(current == rule2.a){
-			nextSentence += rule2.b;
-		} else {
-			nextSentence = current;
+		var found = false;
+		for (var j = 0; j < rules.length; j++) {
+			if (current == rules[j].a) {
+				nextSentence += rules[j].b;
+				break;
+			} 
+		}
+		if (!found) {
+			nextSentence += current;
 		}
 	}
 	sentence = nextSentence;
